7f47336c244cb471a1e0d48cb4bbc009e41258e7
Drew <drew@finstrom.us>
2015-06-19 10:09:38 -0700

    removes deprecation warnings by replacing ObjectController instead of Controller in controller

--- code-review/app/controllers/edit-question.js
+++ code-review/app/controllers/edit-question.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   actions: {
     update: function() {
       var question = this.model;
